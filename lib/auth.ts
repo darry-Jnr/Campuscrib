7ae8e293d36c36d9009fdc19904fd0c948186a67
Oluwadara Sotomi <127672914+darry-Jnr@users.noreply.github.com>
2026-01-01 17:46:26 +0100

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,14 +6,16 @@ import { nextCookies } from "better-auth/next-js";
 const prisma = new PrismaClient()
 
 export const auth = betterAuth({
-    database: prismaAdapter(prisma, {provider: "postgresql"}),
+    baseURL: process.env.BETTER_AUTH_URL, // <-- THIS IS REQUIRED
+
+    database: prismaAdapter(prisma, { provider: "postgresql" }),
 
     emailAndPassword: {
         enabled: true,
         minPasswordLength: 6,
     },
   
-    socialProviders:{
+    socialProviders: {
         google: {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
