0ec22b93090447e89849793424f39895febe808c
Oluwadara Sotomi <127672914+darry-Jnr@users.noreply.github.com>
2026-01-01 17:21:13 +0100

    Update auth-client.ts

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,5 +2,5 @@ import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
     // This tells the client where your Better Auth server is
-    baseURL: process.env.NEXT_PUBLIC_APP_URL;
+    baseURL: process.env.NEXT_PUBLIC_APP_URL,
 });
